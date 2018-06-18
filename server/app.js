46b8e794140cd385cbd2d4b799a2f02f6c5c693d
LaneSun <anlbrain@163.com>
2018-06-18 13:03:26 +0800

    Revert "Change"
    This reverts commit d24832fe0e8dad086d504784cbbd80ecfcecafb7.

--- server/app.js
+++ server/app.js
@@ -2,21 +2,12 @@ var ws = require("nodejs-websocket");
 
 var server = ws.createServer(function (conn){
     //...
-
     conn.sendText(JSON.stringify("Connect Success :)"));
     conn.on("text",function (data) {
         var msg = JSON.parse(data);
-        // console.log(msg);
+        console.log(msg);
         broadcast(data);
-    });
-    conn.on("close", function (code, reason) {
-        console.log("Connection closed")
-    });
-    conn.on("error",function (err) {
-        console.error(err);
-        conn.close()
-    });
-    console.log("New connection")
+    })
 }).listen(9000);
 
 function broadcast(msg) {
